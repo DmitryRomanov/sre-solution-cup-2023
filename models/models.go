9b5358af2723edbde512880a5b78758f0cce993e
Dmitry Romanov <dmitry.romanov85@gmail.com>
2023-04-15 18:33:25 +0300

    common validation rules for add task

--- models/models.go
+++ models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
@@ -19,6 +20,35 @@ type Task struct {
 func (task *Task) ValidateValues() error {
 	if task.Priority == string(TASK_PRIORITY_CRITICAL) && task.Type == string(TASK_TYPE_MANUAL) {
 		return errors.New("приоритет critical (может выставляться только для типа manual)")
+	}
+
+	manualTaskMinuteMultiplicity := 5
+	if task.Type == string(TASK_TYPE_MANUAL) &&
+		task.StartTime.Minute()%manualTaskMinuteMultiplicity != 0 &&
+		task.StartTime.Second() == 0 {
+		return fmt.Errorf("для ручных работ время кратно %v минутам", manualTaskMinuteMultiplicity)
+	}
+
+	autoTaskMinuteMultiplicity := 1
+	if task.Type == string(TASK_TYPE_AUTO) &&
+		task.StartTime.Minute()%autoTaskMinuteMultiplicity != 0 &&
+		task.StartTime.Second() == 0 {
+		return fmt.Errorf("для автоматических работ время кратно %v минутам", autoTaskMinuteMultiplicity)
+	}
+
+	manualTaskMinDurationSeconds := 30 * 60
+	if task.Type == string(TASK_TYPE_MANUAL) && task.Duration < manualTaskMinDurationSeconds {
+		return fmt.Errorf("для ручных работ длительность не меньше %v секунд", manualTaskMinDurationSeconds)
+	}
+
+	autoTaskMinDurationSeconds := 5 * 60
+	if task.Type == string(TASK_TYPE_MANUAL) && task.Duration < autoTaskMinDurationSeconds {
+		return fmt.Errorf("для автоматических работ длительность не меньше %v секунд", autoTaskMinDurationSeconds)
+	}
+
+	criticalTaskMaxDurationSeconds := 6 * 60 * 60
+	if task.Priority == string(TASK_PRIORITY_NORMAL) && task.Duration > criticalTaskMaxDurationSeconds {
+		return fmt.Errorf("длительность не больше %v секунд", criticalTaskMaxDurationSeconds)
 	}
 
 	return nil
